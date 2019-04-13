7ee5835246d582f95c40415348cf7099067a8741
Albert Jeremy Gonzalez <Albert@alberts-mbp.home>
2019-04-13 11:53:28 -0400

    added search bar to maps

--- App.js
+++ App.js
@@ -1,10 +1,31 @@
 import React, {Component} from 'react';
-import MapView from 'react-native-maps'
+import {View, TextInput} from 'react-native';
+import MapView, { Callout } from 'react-native-maps'
 export default class App extends Component<Props> {
     state = {
       latitude: 0,
-            longitude: 0
+      longitude: 0,
+      styles: {
+        calloutView: {
+          flexDirection: `row`,
+          backgroundColor: `rgba(255, 255, 255, 0.9)`,
+          borderRadius: 10,
+          width: `40%`,
+          marginLeft: `30%`,
+          marginRight: `30%`,
+          marginTop: 40
+        },
+        calloutSearch: {
+          borderColor: `transparent`,
+          marginleft: 10,
+          width: `90%`,
+          marginRight: 10,
+          height: 40,
+          borderWidth: 0.0  
+        }
+      }
     }
+    
     componentWillMount() {
       console.log('mounted ')
       navigator.geolocation.getCurrentPosition(
@@ -27,7 +48,10 @@ export default class App extends Component<Props> {
 
 
       return (
-  
+        <View
+        style={{ 
+          height: 1000
+        }}>
         <MapView
           style={{flex: 1}}
           region={{
@@ -38,6 +62,14 @@ export default class App extends Component<Props> {
           }}
           showsUserLocation={true}
         />
+        <Callout>
+    <View style={this.state.styles.calloutView} >
+      <TextInput style={this.state.styles.calloutSearch}
+        placeholder={"Search"}
+      />
+    </View>
+  </Callout>
+      </View> 
       );
    
   }
